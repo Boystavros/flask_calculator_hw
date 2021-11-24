8758bf879e0e195861449487e4b1d882a2fb95e4
Sacha Ponniah <sponniah812@gmail.com>
2021-11-24 17:53:49 +0000

    working with user input numbers, but hardcoded operation.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,11 @@
 from flask import render_template
 from app import app
-from modules.calculator import *
+from modules.calculator import add
 
 
-@app.route('/<operation>/<num_1>/<num_2>')
-def index(operation, num_1, num_2):
-    #return render_template('index.html', calc_operation=operation, first=num_1, second=num_2)
+@app.route('/add/<str_1>/<str_2>')
+def index(str_1, str_2):
+    num_1 = int(str_1)
+    num_2 = int(str_2)
+    result = add(num_1, num_2)
+    return render_template('index.html', output=result)
