321a59b637bc3fa29c526cdcb7d21f1ca0165b13
Sacha Ponniah <sponniah812@gmail.com>
2021-11-24 19:14:57 +0000

    App working as intended with user inputs for operation and numbers.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,11 +1,21 @@
 from flask import render_template
 from app import app
-from modules.calculator import add
+from modules.calculator import add, divide, multiply, subtract
 
 
-@app.route('/add/<str_1>/<str_2>')
-def index(str_1, str_2):
+@app.route('/<operation>/<str_1>/<str_2>')
+def index(operation, str_1, str_2):
     num_1 = int(str_1)
     num_2 = int(str_2)
-    result = add(num_1, num_2)
+    if  operation == 'add':
+        result = add(num_1, num_2)
+    elif operation == 'subtract':
+        result = subtract(num_1, num_2)
+    elif operation == 'multiply':
+        result = multiply(num_1, num_2)
+    elif operation == 'divide':
+        result = divide(num_1, num_2)
+    else:
+        result = "not available"
     return render_template('index.html', output=result)
+
--- modules/calculator.py
+++ modules/calculator.py
@@ -8,4 +8,4 @@ def multiply(num_1, num_2):
     return num_1 * num_2
 
 def divide(num_1, num_2):
-    return num_1 / num_2
+    return num_1 // num_2
