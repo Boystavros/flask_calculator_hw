5c612a435f605ce2b14191c71a00df70dcb368d7
Sacha Ponniah <sponniah812@gmail.com>
2021-11-24 17:34:05 +0000

    File structure for app set up and rendering to localhost.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from app import app
-# from modules.calculator import Calculator
+from modules.calculator import *
 
 
+@app.route('/<operation>/<num_1>/<num_2>')
+def index(operation, num_1, num_2):
+    #return render_template('index.html', calc_operation=operation, first=num_1, second=num_2)
